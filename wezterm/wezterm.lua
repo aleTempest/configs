dbb54101b95b3c22bc2653bd9a269588bf57cee6
ale <ale.edgedancer@gmail.com>
2024-06-02 20:07:45 -0600

    yea

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,17 +2,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- config.color_scheme = "OneDark (base16)"
--- config.color_scheme = "Nord (Gogh)"
--- config.color_scheme = "Oxocarbon Dark (Gogh)"
--- config.font = wezterm.font("Iosevka Nerd Font")
--- config.color_scheme = "Solarized (dark) (terminal.sexy)"
--- config.color_scheme = "Gruvbox Dark (Gogh)"
--- config.color_scheme = "Kanagawa (Gogh)"
 -- config.color_scheme = "Solarized Dark - Patched"
--- config.color_scheme = "Everforest Dark (Gogh)"
--- config.color_scheme = "Everblush (Gogh)"
-config.color_scheme = "Gruvbox Material (Gogh)"
+config.color_scheme = "Solarized Light (Gogh)"
+
 config.font_size = 13.0
 config.enable_tab_bar = false
 
